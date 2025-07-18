cf1a04e62ba9847706f1891ccd2f46f4658dfaad
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-07-18 14:43:46 +0530

    Add Vercel Analytics integration and update dependencies; create drizzle.config.js for database configuration

---
+++ app/drizzle.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+/** @type { import("drizzle-kit").Config } */
+export default {
+  schema: "./utils/schema.js",
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
+  }
+}
